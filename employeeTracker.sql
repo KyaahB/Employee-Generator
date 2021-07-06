a582fb78c83e75181925a30e75fe0c24873533ae
Kyaah <kyaahh@gmail.com>
2021-07-06 15:14:45 -0500

    needs updating

---
+++ employeeTracker.sql
@@ -0,0 +1,115 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) 
+	
+    
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary  DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    
+
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employees(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+
+);
+
+
+INSERT INTO department (d_name) values ('Sales');
+INSERT INTO department (d_name) values ('Engineering');
+INSERT INTO department (d_name) values ('Finance');
+INSERT INTO department (d_name) values ('Legal');
+
+INSERT INTO role (department_id, title, salary ) values (1 , 'Sales Lead', 100000);
+INSERT INTO role (department_id, title, salary ) values ( 1 ,'Salesperson', 80000);
+INSERT INTO role (department_id, title, salary ) values (2,'Lead Engineer', 150000);
+INSERT INTO role (department_id, title, salary ) values (2 ,'Software Engineer', 120000);
+INSERT INTO role (department_id, title, salary ) values (2 , 'Lead Engineer', 125000);
+INSERT INTO role (department_id, title, salary ) values (3 , 'Accountant', 250000);
+INSERT INTO role (department_id, title, salary ) values (4 ,'Legal Team Lead', 190000);
+INSERT INTO role (department_id, title, salary ) values ( 4 ,'Lawyer', 190000);
+
+INSERT INTO employees (first_name, last_name, role_id) values ('Onika', 'Maraj', 1);
+INSERT INTO employees (first_name, last_name, role_id) values ('Jacob', 'Edwards', 2);
+INSERT INTO employees (first_name, last_name, role_id) values ('Stefani', 'Germanotti', 3);
+INSERT INTO employees (first_name, last_name, role_id) values ('Felicity', 'Hudson', 4);
+INSERT INTO employees (first_name, last_name, role_id) values ('Justin', 'Williams', 5);
+INSERT INTO employees (first_name, last_name, role_id) values ('Beyonce', 'Knowles', 6);
+INSERT INTO employees (first_name, last_name, role_id) values ('Trevante', 'Rhodes',7);
+INSERT INTO employees (first_name, last_name, role_id) values ('Lucien', 'Laviscount', 8);
+
+-- ADD DEPARTMENT
+INSERT INTO department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30))
+    VALUES (d_name)
+
+
+
+
+
+-- update employees
+UPDATE [employees]
+SET [role_id] = AUTO_INCREMENT
+WHERE Name = 'updatedEmployee';
+
+
+-- add role
+INSERT INTO role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary  DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    VALUES (department_id, title, salary)
+)
+
+
+
+
+SELECT d_name.department_id, employees.role_id, title.role_id
+FROM department
+INNER JOIN employees ON department.title = employees.role_id
+
+
+SELECT  department.d_name, employees.first_name, employees.last_name, employees.role_id, role.title
+FROM department
+INNER JOIN employees ON department.title = employees.role_id
+
+
+
+
+
+
+
+
+-- SELECT first_name, last_name, role_id
+-- FROM employees
+-- INNER JOIN employees ON employees.employee_id = employee.id;
+
+
+
+
+-- SELECT department,employee_id, title, salary
+-- FROM role
+
+SELECT * FROM role;
+SELECT * FROM department;
+SELECT * FROM employees;
---
+++ employeetracker2.js
@@ -0,0 +1,178 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Your password
+  password: 'Password1',
+  database: 'employeeTracker_DB',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+  });
+
+  const start = () => {
+      inquirer
+      .prompt({
+        name: 'toDo',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'View All Employees By Department',
+            'Add Department',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles'
+
+        ]
+      })
+    
+
+  .then((answer) => {
+      switch (answer.toDo) {
+          case 'View All Employees':
+          viewEmployees();
+          break;
+
+          case 'View All Employees By Department':
+          viewDepartment();
+          break;
+
+          case 'Add Department':
+          addDepartment();
+          break;
+
+          case 'Add Role':
+          addRole();
+          break;
+
+          case 'Add Employee':
+          addEmployee();
+          break;
+
+          case 'Update Employee Role':
+          update();
+          break;
+
+          case 'View All Roles':
+          viewRoles();
+          break;
+
+          default:
+              console.log(`Invalid action: ${answer.action}`);
+              break;
+      }
+  })};
+
+//   get table from emploee table to appear in console (console.table)
+  const viewEmployees = () => {
+     connection.query(
+         'SELECT * FROM employees',
+         function(err, result){
+             console.table(result);
+             start();
+         })
+    };
+
+   const viewDepartment = () => {
+    connection.query(
+        'SELECT * FROM department',
+        function(err, result){
+            console.table(result)
+            
+        })  
+    };
+
+   const addDepartment = () => {
+    connection.query(
+        'SELECT * FROM department',
+        function(err, result){
+            console.table(result)
+            
+        })  
+    };
+
+   const addRole = () => {
+    connection.query(
+        'SELECT * FROM department',
+        function(err, result){
+            console.table(result)
+
+
+            const updateRole = () => {
+                console.log('Updating all Rocky Road quantities...\n');
+                const query = connection.query(
+                  'UPDATE products SET ? WHERE ?',
+                  // set correlates to the first object(quantity) and where correlates to (flavor)
+                  [
+                    {
+                      quantity: 100,
+                    },
+                    {
+                      flavor: 'Rocky Road',
+                    },
+                  ],
+                  (err, res) => {
+                    if (err) throw err;
+                    console.log(`${res.affectedRows} products updated!\n`);
+                    // Call deleteProduct AFTER the UPDATE completes
+                    // deleteProduct();
+                  }
+                );
+              
+                // logs the actual query being run
+                console.log(query.sql);
+              };
+            
+
+
+
+
+
+
+        })  
+    };
+       
+    
+    const addEmployee = () => {
+        connection.query(
+            'SELECT * FROM role',
+            function(err, result){
+                console.table(result)
+                
+        })
+    };
+
+    const updateEmployee = () => {
+        connection.query(
+            'SELECT * FROM employees',
+            function(err, result){
+                console.table(result)
+                
+        })
+    };
+
+    const viewRoles = () => {
+        connection.query(
+            'SELECT * FROM role',
+            function(err, result){
+                console.table(result)
+                
+        })
+    };
+       
+    
+
+  
+
